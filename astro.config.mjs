833ce57570c7c75bda67837439f7cd92f49f9955
nematatu <nematatu1003@gmail.com>
2026-01-31 13:27:36 +0900

    fix: p

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,6 +13,9 @@ export default defineConfig({
 	integrations: [sitemap(), mdx(), pagefind()],
 	image: {
 		domains: ["assets.blog.amatatu.com"],
+		service: {
+			entrypoint: "astro/assets/services/noop",
+		},
 	},
 	vite: {
 		plugins: [tailwindcss()],
